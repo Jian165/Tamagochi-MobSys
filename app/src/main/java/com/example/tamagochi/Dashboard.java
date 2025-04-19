3cea5f0012868f5b31b701c1a9e6a70de9d1db0f
Jian <duranjhonmark2@gmail.com>
2025-04-19 13:11:19 +0800

    finish food progress, happiness progress bar, dead animation, ball game animation,

--- app/src/main/java/com/example/tamagochi/Dashboard.java
+++ app/src/main/java/com/example/tamagochi/Dashboard.java
@@ -1,11 +1,15 @@
 package com.example.tamagochi;
-
 import android.content.ClipData;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,17 +21,19 @@ import java.util.ArrayList;
 
 public class Dashboard extends AppCompatActivity {
 
-    ImageView characterImg,
-            shit1Img,
-            shit2Img,
-            shit3Img,
-            shit4Img,
-            shit5Img,
-            feedImg,
-            cleanImg;
-    private Runnable runnable;
-    private ArrayList<Integer> charachterIdleImages;
-    private int IdleImgIndex = 0;
+    private ImageView characterImg, shit1Img, shit2Img, shit3Img, shit4Img, shit5Img, feedImg, cleanImg,shadowImg,playImg,playballImg;
+    private ProgressBar healthProg, happinessProg;
+    private FrameLayout playBallArea;
+
+    private float velocity = 0, gravity = 0.7f, jumpStrangth = -16f;
+    private int screenHeight;
+    private boolean isIdle = false, isPlayBallIn = false;
+    private Runnable runnable,ballGameLoop;
+    private ArrayList<Integer> charachterIdleImages,characterDeadImages,shitCollected;
+    private int idleImgIndex = 0,deadImgIndex = 0;
+
+    private boolean isCharacterAlive;
+
     private Handler handler =  new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +47,7 @@ public class Dashboard extends AppCompatActivity {
         });
         hideSystemUI();
         LoadComponents();
-        PlayIdle();
-
+        CheckHealth();
     }
 
     private View.OnLongClickListener itemClickListiner(){
@@ -71,6 +76,9 @@ public class Dashboard extends AppCompatActivity {
                    case DragEvent.ACTION_DROP:
                        final View view = (View) event.getLocalState();
                        if(view.getId() == R.id.imgClean) {
+                           if(!shitCollected.contains(shitImage.getId())){
+                               shitCollected.add(shitImage.getId());
+                           }
                            shitImage.setAlpha(0f);
                        }
                        break;
@@ -79,27 +87,84 @@ public class Dashboard extends AppCompatActivity {
            }
        };
     }
-    private void LoadComponents(){
-        characterImg = findViewById(R.id.imgCharacter);
-        feedImg  = findViewById(R.id.imgFeed);
-        cleanImg = findViewById(R.id.imgClean);
-        shit1Img = findViewById(R.id.imgShit1);
-        shit2Img = findViewById(R.id.imgShit2);
-        shit3Img = findViewById(R.id.imgShit3);
-        shit4Img = findViewById(R.id.imgShit4);
-        shit5Img = findViewById(R.id.imgShit5);
-
-        cleanImg.setOnLongClickListener(itemClickListiner());
-        feedImg.setOnLongClickListener(itemClickListiner());
-        shit1Img.setOnDragListener(shitDragListiner(shit1Img));
-        shit2Img.setOnDragListener(shitDragListiner(shit2Img));
-        shit3Img.setOnDragListener(shitDragListiner(shit3Img));
-        shit4Img.setOnDragListener(shitDragListiner(shit4Img));
-        shit5Img.setOnDragListener(shitDragListiner(shit5Img));
+    private View.OnTouchListener BallAreaListener(){
+        return new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction()==MotionEvent.ACTION_DOWN){
+                    velocity=jumpStrangth;
+                    if(isCharacterAlive){
+                        HappinessIncrease(1*shitCollected.size());
+                    }
+                }
+                return true;
+            }
+        };
+    }
+    private View.OnDragListener CharacterListener(ImageView characterImg){
+        return new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                int dragEvent =  event.getAction();
+                final View view = (View) event.getLocalState();
+                switch (dragEvent){
+                    case DragEvent.ACTION_DRAG_ENTERED:
+                        if(view.getId() == R.id.imgFeed) {
+                           stopIdle();
+                           characterImg.setImageResource(R.drawable.eating);
+                        }
+                        break;
+                    case DragEvent.ACTION_DRAG_EXITED:
+                        if(view.getId() == R.id.imgFeed) {
+                            CheckHealth();
+                        }
+                        break;
+                    case DragEvent.ACTION_DROP:
+                        if(view.getId() == R.id.imgFeed) {
+                            HealthIncrease(10);
+                            CheckHealth();
+                        }
+                        if(view.getId()==R.id.imgPlay){
+                            if(!isPlayBallIn){
+                                playBallArea.setVisibility(View.VISIBLE);
+                                playBallArea.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        playballImg.setY(0);
+                                        velocity=0;
+                                        isPlayBallIn = true;
+                                        screenHeight = playBallArea.getHeight();
+                                        StartBallGame();
+                                    }
+                                });
+                            }
+                        }
+                        break;
+                }
+                return true;
+            }
+        };
     }
 
-    private void PlayIdle()
+    private void CheckHealth(){
+        if(healthProg.getProgress()<=10){
+            DeadAnimation();
+        }
+        else{
+            PlayIdle();
+        }
+    }
+
+    private void HappinessIncrease(int happy)
     {
+        happinessProg.setProgress(happinessProg.getProgress()+happy);
+    }
+
+    private void HealthIncrease(int health)
+    {
+        healthProg.setProgress(healthProg.getProgress()+health);
+    }
+    private void LoadComponents(){
         charachterIdleImages = new ArrayList<>();
         charachterIdleImages.add(R.drawable.idle_1);
         charachterIdleImages.add(R.drawable.idle_2);
@@ -112,15 +177,144 @@ public class Dashboard extends AppCompatActivity {
         charachterIdleImages.add(R.drawable.idle_9);
         charachterIdleImages.add(R.drawable.idle_10);
 
+        characterDeadImages = new ArrayList<>();
+        characterDeadImages.add(R.drawable.dead_1);
+        characterDeadImages.add(R.drawable.dead_2);
+        characterDeadImages.add(R.drawable.dead_3);
+        characterDeadImages.add(R.drawable.dead_4);
+        characterDeadImages.add(R.drawable.dead_5);
+        characterDeadImages.add(R.drawable.dead_6);
+        characterDeadImages.add(R.drawable.dead_7);
+        characterDeadImages.add(R.drawable.dead_8);
+        characterDeadImages.add(R.drawable.dead_9);
+        characterDeadImages.add(R.drawable.dead_10);
+
+        shitCollected = new ArrayList<>();
+
+        characterImg = findViewById(R.id.imgCharacter);
+        feedImg  = findViewById(R.id.imgFeed);
+        cleanImg = findViewById(R.id.imgClean);
+        shit1Img = findViewById(R.id.imgShit1);
+        shit2Img = findViewById(R.id.imgShit2);
+        shit3Img = findViewById(R.id.imgShit3);
+        shit4Img = findViewById(R.id.imgShit4);
+        shit5Img = findViewById(R.id.imgShit5);
+        shadowImg = findViewById(R.id.imgShadow);
+        healthProg =  findViewById(R.id.healthProgress);
+        happinessProg =  findViewById(R.id.happinessProgress);
+        playImg = findViewById(R.id.imgPlay);
+        playballImg = findViewById(R.id.imgPlayBall);
+        playBallArea= findViewById(R.id.fLayout_BallGame);
+
+        cleanImg.setOnLongClickListener(itemClickListiner());
+        feedImg.setOnLongClickListener(itemClickListiner());
+        playImg.setOnLongClickListener(itemClickListiner());
+        playBallArea.setOnTouchListener(BallAreaListener());
+
+        characterImg.setOnDragListener(CharacterListener(characterImg));
+        shit1Img.setOnDragListener(shitDragListiner(shit1Img));
+        shit2Img.setOnDragListener(shitDragListiner(shit2Img));
+        shit3Img.setOnDragListener(shitDragListiner(shit3Img));
+        shit4Img.setOnDragListener(shitDragListiner(shit4Img));
+        shit5Img.setOnDragListener(shitDragListiner(shit5Img));
+
+        playBallArea.setVisibility(View.GONE);
+
+    }
+
+    private void StartBallGame(){
+        ballGameLoop = new Runnable() {
+            @Override
+            public void run() {
+                boolean isBallGraounded = false;
+                float y = playballImg.getY();
+                velocity += gravity;
+                y += velocity;
+
+                if(y+playballImg.getHeight()>screenHeight){
+                    y=screenHeight-playballImg.getHeight();
+                    velocity=0;
+                    isBallGraounded = true;
+                }
+
+                if(y<0){
+                    y=0;
+                    velocity=0;
+                }
+                if(!isBallGraounded){
+                    playballImg.setY(y);
+                    handler.postDelayed(this,16);
+                }
+                else{
+                    BallGameOver();
+                }
+            }
+        };
+        handler.post(ballGameLoop);
+    }
+
+    private void BallGameOver()
+    {
+        handler.removeCallbacks(ballGameLoop);
+        playBallArea.setVisibility(View.GONE);
+        isPlayBallIn = false;
+    }
+
+
+    private void PlayIdle()
+    {
+        isCharacterAlive=true;
+        adjustShadow(30);
+        if(!isIdle) {
+            runnable = new Runnable() {
+                @Override
+                public void run() {
+                    characterImg.setImageResource(charachterIdleImages.get(idleImgIndex));
+                    idleImgIndex = (idleImgIndex +1)%charachterIdleImages.size();
+                    handler.postDelayed(this,60);
+                }
+            };
+            isIdle = true;
+            handler.post(runnable);
+        }
+    }
+    private void adjustShadow(int shadowMargin){
+        int dp = (int) (shadowMargin * getResources().getDisplayMetrics().density + 0.5f);
+        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) shadowImg.getLayoutParams();
+        params.setMargins(params.leftMargin,params.topMargin,params.rightMargin,dp);
+        shadowImg.setLayoutParams(params);
+    }
+
+    private void DeadAnimation()
+    {
+        isCharacterAlive = false;
+        adjustShadow(40);
+        if(isIdle){
+            stopIdle();
+            isIdle = false;
+        }
         runnable = new Runnable() {
             @Override
             public void run() {
-                characterImg.setImageResource(charachterIdleImages.get(IdleImgIndex));
-                IdleImgIndex = (IdleImgIndex+1)%charachterIdleImages.size();
-                handler.postDelayed(this,60);
+                characterImg.setImageResource(characterDeadImages.get(deadImgIndex));
+                if(deadImgIndex+1<characterDeadImages.size())
+                {
+                    deadImgIndex++;
+                    handler.postDelayed(this,60);
+                }
+                else{
+                    handler.removeCallbacks(runnable);
+                }
             }
         };
         handler.post(runnable);
+    }
+
+    private void stopIdle(){
+        if(isIdle) {
+            handler.removeCallbacks(runnable);
+            isIdle = false;
+        }
     }
     private void hideSystemUI() {
         View decorView = getWindow().getDecorView();
